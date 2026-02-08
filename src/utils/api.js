54293cd9a6009aaa55900a610af0c5aad8b8e96c
kkhhjjoo <khjoo1203@gmail.com>
2026-02-08 16:28:48 +0900

    api 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,41 @@
+// Netlify 프록시를 사용하도록 수정된 api.js
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+  baseURL: '/api',  // 상대 경로로 변경 (Netlify 프록시 사용)
   headers: {
     "Content-Type": "application/json",
-    authorization: "Bearer " + localStorage.getItem("token"),
   },
 });
+
 /**
- * console.log all requests and responses
+ * Interceptor: 요청 전 authorization 헤더에 토큰 추가
  */
 api.interceptors.request.use(
   (request) => {
     console.log("Starting Request", request);
+    request.headers.authorization = "Bearer " + sessionStorage.getItem("token");
     return request;
   },
   function (error) {
     console.log("REQUEST ERROR", error);
+    return Promise.reject(error);
   }
 );
 
+/**
+ * Interceptor: 응답 처리
+ */
 api.interceptors.response.use(
-  (response) => {
-    console.log("Response:", response);
+  function (response) {
+    console.log("RESPONSE", response);
     return response;
   },
   function (error) {
-    error = error.response ? error.response.data : { message: error.message };
     console.log("RESPONSE ERROR", error);
-    return Promise.reject(error);
+    // 에러 객체 반환 (에러 처리 개선)
+    return Promise.reject(error.response ? error.response.data : error);
   }
 );
 
