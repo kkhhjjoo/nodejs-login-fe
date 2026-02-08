0dc1a330a2c7cf3e5e5a25252eb9604e7b62277c
kkhhjjoo <khjoo1203@gmail.com>
2026-02-08 16:37:02 +0900

    넷리파이 리다이렉트 설정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,41 +1,34 @@
-// Netlify 프록시를 사용하도록 수정된 api.js
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',  // 상대 경로로 변경 (Netlify 프록시 사용)
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
   headers: {
     "Content-Type": "application/json",
+    authorization: "Bearer " + localStorage.getItem("token"),
   },
 });
-
 /**
- * Interceptor: 요청 전 authorization 헤더에 토큰 추가
+ * console.log all requests and responses
  */
 api.interceptors.request.use(
   (request) => {
     console.log("Starting Request", request);
-    request.headers.authorization = "Bearer " + sessionStorage.getItem("token");
     return request;
   },
   function (error) {
     console.log("REQUEST ERROR", error);
-    return Promise.reject(error);
   }
 );
 
-/**
- * Interceptor: 응답 처리
- */
 api.interceptors.response.use(
-  function (response) {
-    console.log("RESPONSE", response);
+  (response) => {
+    console.log("Response:", response);
     return response;
   },
   function (error) {
+    error = error.response.data;
     console.log("RESPONSE ERROR", error);
-    // 에러 객체 반환 (에러 처리 개선)
-    return Promise.reject(error.response ? error.response.data : error);
+    return Promise.reject(error);
   }
 );
 
